d3eddf6742416651aa1697178e2c638161ea0fb0
一个小瘪三 <10948399+menon-qiqi@user.noreply.gitee.com>
2023-07-12 22:53:40 +0800

    ci: deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React test github actions
         </a>
       </header>
     </div>
